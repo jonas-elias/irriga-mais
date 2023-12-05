c43d14e95ade07de22239948595e8da422101259
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-12-05 01:14:44 -0300

    test: remove import

--- irriga_mais/irrigacao/tests.py
+++ irriga_mais/irrigacao/tests.py
@@ -3,7 +3,6 @@ import json
 from datetime import datetime, timedelta
 from django.test import TestCase, RequestFactory
 from django.contrib.auth.hashers import make_password
-from django.urls import reverse
 from .models import Fazenda, PivoIrrigacao, EventoIrrigacao, TokenAuth
 from .views import onOffPivo, getAllFazendas, getFazendaById, createFazenda, loginFazenda, createPivo, deletePivo, getAllPivos, createEvento, deleteEvento, updateEventoIrrigacao, getIrrigacaoEvento
 
